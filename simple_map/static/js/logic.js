b274546bab223143a5abd72d17dd0a1bee070106
Danny <89036003+daboucha@users.noreply.github.com>
2021-11-15 16:14:31 -0500

    Update

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -26,5 +26,9 @@ let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(city => {
-    L.marker(city.location).addTo(map);
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
 });
