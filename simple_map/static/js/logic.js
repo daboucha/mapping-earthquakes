a1fb6ab8a11aa58674296d149718c7beed12d214
Danny <89036003+daboucha@users.noreply.github.com>
2021-11-17 12:25:18 -0500

    Update

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -1,5 +1,8 @@
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// Create the map object with center at the San Francisco airport
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -21,14 +24,53 @@ let streets = L.tileLayer('https://api.mapbox.com/{style}/tiles/{z}/{x}/{y}?acce
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// // Get data from cities.js
+// let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(city => {
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(city => {
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+//       return L.marker(latlng)
+//       .bindPopup("<h2>" + feature.properties.city + "</h2>")
+//     }
+//   }).addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+  }).addTo(map);
